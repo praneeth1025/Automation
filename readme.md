6d0e3062d5b3735d12e77dcf460cb539ee2607d5
shivacharannamala <shivacharan.namala@phenom.com>
2025-02-19 00:21:55 +0530

    python selenium

---
+++ readme.md
@@ -0,0 +1,11 @@
+Use the following to create virtual env.
+python3 -m venv venv
+
+Activate virtual env
+source venv/bin/activate
+
+Install all the requirements
+pip install -r requirements.txt
+
+Then run the following
+python3 main.py (In Ubuntu)
---
+++ scripts/constants.py
@@ -0,0 +1,20 @@
+"""
+Module to contain the constants for applications
+"""
+
+
+class BrowserConstants:
+    def __init__(self):
+        self.url_jobs_linkedin = 'https://www.linkedin.com/jobs'
+
+
+class ApplicantConstants:
+    def __init__(self):
+        ## username and passwords
+        self.username = 'namalashivacharan123'
+        self.email_id = 'namalashivacharan123@gmail.com'
+        self.password = 'password@123'
+
+        ## education
+        self.degree_college = 'JNTUH College of Engineering Sultanpur'
+        self.inter_college = 'Shraddha Junior College'
